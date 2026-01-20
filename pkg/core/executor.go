3950ede59887d007a30f2a97aae30fccc2a71079
Guilherme Raduenz <graduenz@gmail.com>
2026-01-20 12:32:05 -0300

    made methods with context overload available in SQLExecutor

--- pkg/core/executor.go
+++ pkg/core/executor.go
@@ -1,8 +1,14 @@
 package core
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
+// SQLExecutor defines the interface for executing SQL commands, relying on database/sql package.
 type SQLExecutor interface {
 	Exec(query string, args ...any) (sql.Result, error)
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	Query(query string, args ...any) (*sql.Rows, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 }
