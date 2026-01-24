d6e1c0c700a15858778566dedb7b2abfa24b9eeb
Guilherme Raduenz <graduenz@gmail.com>
2026-01-24 15:53:38 -0300

    added some schema tests

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -3,9 +3,24 @@ package schema_test
 import (
 	"testing"
 
+	"github.com/ordershift/ormshift"
+	"github.com/ordershift/ormshift/dialects/sqlite"
 	"github.com/ordershift/ormshift/internal/testutils"
 	"github.com/ordershift/ormshift/schema"
 )
+
+func TestNewDBSchema(t *testing.T) {
+	lDB, lError := ormshift.OpenDatabase(sqlite.SQLiteDriver{}, ormshift.ConnectionParams{InMemory: true})
+	if !testutils.AssertNilError(t, lError, "ormshift.OpenDatabase") {
+		return
+	}
+	defer func() { _ = lDB.Close() }()
+
+	lDBSchema, lError := schema.NewDBSchema(lDB.DB(), "query")
+	if !testutils.AssertNotNilResultAndNilError(t, lDBSchema, lError, "schema.NewDBSchema") {
+		return
+	}
+}
 
 func TestNewDBSchemaFailsWhenDBIsNil(t *testing.T) {
 	lDBSchema, lError := schema.NewDBSchema(nil, "query")
